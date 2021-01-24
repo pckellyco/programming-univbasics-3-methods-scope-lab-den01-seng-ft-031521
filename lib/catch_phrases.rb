caaff4fee5bc3fc30ea682aafa6f2a4c004a7975
Paige Kelly <pckelly.co@gmail.com>
2021-01-24 20:53:11 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,23 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
+  phrase
 end
 
 def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
+  status
+end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+  phrase
+end
+
+def all_phrases
+  let phrases = mario toadstool lin
+  puts mario toadstool link
+  mario toadstool link
 end
